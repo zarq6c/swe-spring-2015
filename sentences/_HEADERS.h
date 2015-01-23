f5a1061e6913331649f7c429d540392cfac02079
BenWool <bennywool@yahoo.com>
2015-01-23 14:37:48 -0600

    Excercise 1
    Ben Woolridge

--- sentences/_HEADERS.h
+++ sentences/_HEADERS.h
@@ -1,6 +1,8 @@
 // DECLARE Function Symbols
 int bensSentence_1();
 int bensSentence_FINAL();
+int bmwr84();
+int bmwr84_second();
 
 // DECLARE YOUR symbols here, implement them in a standalone C file
 
---
+++ sentences/bmwr84.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+////functions
+int bmwr84(){
+	printf("There was a dragon,");
+	return 1;
+	}
+	
+int	bmwr84_second(){
+	printf("The dragon breathed his firey breath");
+	return 1;
+	}
