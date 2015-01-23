69f8edf10ddc3706cbfcfb6b74a0c2b35362ebf1
Grant Scott <GrantScott@missouri.edu>
2015-01-23 00:07:37 -0600

    Initial Files

---
+++ sentences/_HEADERS.h
@@ -0,0 +1,6 @@
+// DECLARE Function Symbols
+int bensSentence_1();
+int bensSentence_FINAL();
+
+// DECLARE YOUR symbols here, implement them in a standalone C file
+
---
+++ story.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "sentences/_HEADERS.h"
+
+// I assume all of you know C.
+
+int main( void )
+{
+
+  printf("\n\n");
+  bensSentence1();
+  printf("\n\n");
+
+  // Add your sentence.
+  // Don't forget to update the header file.
+
+
+
+  // Bens sentence should be last.
+  bensSentence_FINAL();
+
+  printf("\n\n");
+}
+
