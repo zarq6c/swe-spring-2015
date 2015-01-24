904d8eaff5a3e23d403334d3a28fa95d44bfe63c
Alex Drake <amd2b7@mail.misouri.edu>
2015-01-24 13:42:02 -0600

    amd2b7-itll work eventually...

---
+++ sentences/amd2b7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int amd2b7_sentence1(){
+printf("A noble hero stumbled upon the frozen foes and thought to himself, “If only I were 20 years younger, I could slay these here vermin and relive my days of glory!” ");
+return 1;
+}
+
+int amd2b7_sentence2(){
+printf("Little did he know that Agent Buckwald the wizard had secretly been working on to form a Link to the Past.");
+return 1;
+}
