3ca0cc33f06e82d182f06224d90722a7694f3957
Devin Clark <dc4g2@mail.missouri.edu>
2015-01-24 14:51:22 -0600

    Added story, fixed someone's formatting

---
+++ sentences/dc4g2.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+
+// Function to include & executeA
+int devinsSentence1()
+{
+  printf("\n\nThe noble hero, Zelda, and the wizard formed an allegiance and began work on a Temple of Time.\n");
+  return 1;
+}
+
+int devinsSentence2()
+{
+  printf("It was in this temple of time the two hoped they could send our noble hero in his green pointed hat back in time. \n");
+  return 1;
+}
