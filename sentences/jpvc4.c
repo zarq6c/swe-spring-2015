03194d17175d8e4839f1e4be0b0f45614e045f62
Jake Parham <jakeparham@Jakes-MacBook-Pro.local>
2015-01-23 20:24:13 -0600

    added file with my paw print

---
+++ sentences/jpvc4.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+
+int jpvc4_Sentence1()
+{
+	printf("Then, Agent Buckwald the Wizard appeared and froze the dragon and Dover Demon.\n");
+	return 1;
+}
+
+int jpvc4_Sentence2()
+{
+	printf("The wizard used all of his energy keeping them frozen, waiting until a worthy hero would rise up to slay them.\n");
+	return 1;
+}
