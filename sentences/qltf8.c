3dc8831248902f36ccbf15ba70aae65855afc9c6
Liu,Qintai <qltf8@mail.missouri.edu>
2015-01-26 10:37:51 -0600

    modified

--- sentences/qltf8.c
+++ sentences/qltf8.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int qltf8(){
-	printf("\n%s","I know I have to wirte something in there,");
+	printf("\n%s","Agent Buckwald thought a while,");
 	return 1;
 	}
 
 int qltf8_next(){
-	printf("%s\n","however, I can not figure out what i should write to continue this story");
+	printf("%s\n","and then he shocked");
 	return 1;
 }
 
