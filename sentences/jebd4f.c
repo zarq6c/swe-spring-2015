4c23d8a4dd78dc3de825a21e1b0d905364a57d7d
Jayme Becker <jebd4f@mail.missouri.edu>
2015-01-25 21:28:13 -0600

    I added my file to the sentences directory

---
+++ sentences/jebd4f.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int jebd4f_Sentence1()
+{
+	printf("The reader soon realized that this was not a video game, but the story must continue....");
+	return 1;
+}
+
+
+int jebd4f_Sentence2()
+{
+	printf("Agent Buckwald soon realized that it wasn't Link that he was dealing with, but it was the Sasquatch");
+	return 1;
+}
