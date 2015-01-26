233976ef75b687899d90382bde7f3190b12d0e10
India Hampton <iphvg5@mail.missouri.edu>
2015-01-26 11:16:16 -0600

    Just modified file to put in a new line to start the sentence

--- sentences/iphvg5.c
+++ sentences/iphvg5.c
@@ -2,6 +2,6 @@
 
 int iphvg5_sentence1()
 {
-		printf("Sasquatch was really a member of the hairy humanoids.\n");
+		printf("\n Sasquatch was really a member of the hairy humanoids.\n");
 			return 1;
 }
