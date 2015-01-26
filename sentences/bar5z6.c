eca98c0fe5d080861b0e5d77db9d25516e479fd6
Benjamin Rogers <bar5z6@mail.missouri.edu>
2015-01-25 20:48:48 -0600

    Added 2 new sentences to story.c. Also commented out user jebd4f due to missing c file, which stopped the compile

---
+++ sentences/bar5z6.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+
+// Function to include & executeA
+ int bar5z6Sentence1()
+ {
+   printf("To pass the time, the wizard decided to do some logic design, because he felt like he needed to further add to his insanity.\n");
+     return 1;
+  }
+
+ int bar5z6Sentence2()
+ {
+  printf("\nZelda replied...Uhh, I do not think you know what that word means.\n");
+  return 1;
+ }
+
