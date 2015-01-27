0d6dc521941af7de5af20ae22832d574f63d0218
kilan <kilan@mwc-060181.dhcp.missouri.edu>
2015-01-27 14:24:47 -0600

    This is the .c file for zzff8

---
+++ sentences/zzff8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int zzff8_sentence1(){
+	printf("\n%s","this is just the test");
+	return 1;
+	}
+
+int zzff8_sentence2(){
+	printf("%s\n","and he walk around");
+	return 1;
+}
+
