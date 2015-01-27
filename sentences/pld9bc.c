a86a56dd08bc3bcc46b236d330e6fa39fbe19998
PDirks- <pld9bc@mail.missouri.edu>
2015-01-26 21:00:24 -0600

    added pld9bc's two sentences to the story

---
+++ sentences/pld9bc.c
@@ -0,0 +1,17 @@
+/*
+ * Peter Dirks
+ * pld9bc
+ * assignment 1
+ */
+
+# include <stdio.h>
+ 
+int pld9bc_1(){
+	printf(" This project wasn't using the GPL public license, bringing anger to the wise wizard Richard M Stallman.");
+	return 1;
+}
+
+int pld9bc_2(){
+	printf("Thunder rumbled, and one could almost hear someone whisper \"Only GNU will protect you.\" ");
+	return 1;
+}
