8f06df6409834c25f60710df7ad29bdef368fdec
Kyle McCarthy <km@kylemccarthy.io>
2015-01-27 14:26:35 -0600

    fixed indentation

--- story.c
+++ story.c
@@ -5,56 +5,55 @@
 
 int main( void )
 {
+    printf("\n\n");
+    bensSentence1();
+    printf("\n\n");
 
-  printf("\n\n");
-  bensSentence1();
-  printf("\n\n");
-
-  // Add your sentence.
-  // Don't forget to update the header file.
-  bmwr84();
-  printf(" ");
-  darrensSentence1();
-  iphvg5_sentence2();
-  lmwv2c_first();
-  bmwr84_second();
-  darrensSentence2();
-  tnrn9b1();
-  tnrn9b2();
-  jpvc4_Sentence1();
-  bar5z6Sentence1();
-  jpvc4_Sentence2();
-  amd2b7_sentence1();
-  pld9bc_2();
-  amd2b7_sentence2();
-  pld9bc_1();
-
-
-  devinsSentence1();
-  jebd4f_Sentence1();
-  devinsSentence2();
-
-  kllxvd();
-  kllxvd2();
-
-  bar5z6Sentence2();
-  lmwv2c_second();
-
-  qltf8();
-  qltf8_next();
-
-//  jmlmdf();
-  //jmlmdfnext();
-jebd4f_Sentence2();
-iphvg5_sentence1();
-
-  //blhtkd_sentence1();
-  //blhtkd_sentence2();
+    // Add your sentence.
+    // Don't forget to update the header file.
+    bmwr84();
+    printf(" ");
+    darrensSentence1();
+    iphvg5_sentence2();
+    lmwv2c_first();
+    bmwr84_second();
+    darrensSentence2();
+    tnrn9b1();
+    tnrn9b2();
+    jpvc4_Sentence1();
+    bar5z6Sentence1();
+    jpvc4_Sentence2();
+    amd2b7_sentence1();
+    pld9bc_2();
+    amd2b7_sentence2();
+    pld9bc_1();
 
 
-  // Bens sentence should be last.
-  bensSentence_FINAL();
+    devinsSentence1();
+    jebd4f_Sentence1();
+    devinsSentence2();
 
-  printf("\n\n");
+    kllxvd();
+    kllxvd2();
+
+    bar5z6Sentence2();
+    lmwv2c_second();
+
+    qltf8();
+    qltf8_next();
+
+    //  jmlmdf();
+    //jmlmdfnext();
+    jebd4f_Sentence2();
+    iphvg5_sentence1();
+
+    //blhtkd_sentence1();
+    //blhtkd_sentence2();
+
+
+    // Bens sentence should be last.
+    bensSentence_FINAL();
+
+    printf("\n\n");
 }
 
