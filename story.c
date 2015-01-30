43f7e98c1ee168ca1bb07ae179386fc973ec1dfe
bjt2p3_112816 <bjt2p3@babbage.cs.missouri.edu>
2015-01-29 20:21:07 -0600

    updated story.c

--- story.c
+++ story.c
@@ -177,8 +177,8 @@ int main( void )
 	edw8n8_SENTENCE2();
 	wjbz82_second();
 	vlb5b_second(); 
-//	bjt2p3_print();
-//	bjt2p3_next();	
+	bjt2p3_print();
+	bjt2p3_next();	
 //    ifh2k6_1();	//file missing
 //    ifh2k6_2();
 
