af81d66f78ed911e27ab18d0f3a58928ff40645b
David Boullion <Arquad@outlook.com>
2015-01-29 18:05:54 -0600

    Almost forgot my file (drby96.c)

---
+++ sentences/drby96.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//David Boullion
+//drby96
+
+int drby96firstsentence(){
+printf("At this point, Mindor Dorfmann, Samurai King of the Dwarves, whom had been observing all that had happened, decided that this was all far too silly, and sealed his fortress off from the outside world. ");
+return 1;
+}
+
+int drby96secondsentence(){
+printf("Unfortunately, Agent Buckwald's actions had gone too far, provoking the remotely-located and totally secret Reptoid Illuminati to unleash their secret weapon! ");
+return 1;
+}
